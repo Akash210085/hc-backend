11f10f79d79d0dbe7fd8df084a2f67f288bf8dba
Akash Kumar <ak8923307022@gmail.com>
2024-05-30 13:31:28 +0530

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,156 @@
+const jwt = require("jsonwebtoken");
+const otpGenerator = require("otp-generator");
+const User = require("../models/user");
+const filterObj = require("../utils/filterObj");
+
+const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
+
+//Register New User
+exports.register = async (req, res, next) => {
+  const { name, rollNo, email, password } = req.body;
+
+  const filteredBody = filterObj(
+    req.body,
+    "name",
+    "rollNo",
+    "email",
+    "password"
+  );
+  //check if a verfied user with given email exists
+
+  const existing_user = await User.findOne({ email: email });
+
+  if (existing_user && existing_user.verified) {
+    res.status(400).json({
+      status: "error",
+      message: "Email is already in use, Please login.",
+    });
+  } else if (existing_user) {
+    // if not verified than update prev one
+
+    await User.findOneAndUpdate({ email: email }, filteredBody, {
+      new: true,
+      validateModifiedOnly: true,
+    });
+
+    // generate an otp and send to email
+    req.userId = existing_user._id;
+    next();
+  } else {
+    // if user is not created before than create a new one
+    const new_user = await User.create(filteredBody);
+
+    // generate an otp and send to email
+    req.userId = new_user._id;
+    next();
+  }
+};
+
+exports.sendOTP = async (req, res, next) => {
+  const { userId } = req;
+  const new_otp = otpGenerator.generate(6, {
+    upperCaseAlphabets: false,
+    specialChars: false,
+    lowerCaseAlphabets: false,
+  });
+
+  const otp_expiry_time = Date.now() + 10 * 60 * 1000; // 10 Mins after otp is sent
+
+  const user = await User.findByIdAndUpdate(userId, {
+    otp_expiry_time: otp_expiry_time,
+  });
+
+  user.otp = new_otp.toString();
+
+  await user.save({ new: true, validateModifiedOnly: true });
+
+  console.log(new_otp);
+
+  // TODO send mail
+  mailService.sendEmail({
+    from: "ak8923307022@gmail.com",
+    to: user.email,
+    subject: "Verification OTP",
+    html: otp(user.name, new_otp),
+    attachments: [],
+  });
+
+  res.status(200).json({
+    status: "success",
+    message: "OTP Sent Successfully!",
+  });
+};
+
+exports.verifyOTP = async (req, res, next) => {
+  // verify otp and update user accordingly
+  const { email, otp } = req.body;
+  const user = await User.findOne({
+    email,
+    otp_expiry_time: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email is invalid or OTP expired",
+    });
+  }
+
+  if (user.verified) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email is already verified",
+    });
+  }
+
+  if (!(await user.correctOTP(otp, user.otp))) {
+    res.status(400).json({
+      status: "error",
+      message: "OTP is incorrect",
+    });
+
+    return;
+  }
+
+  // OTP is correct
+
+  user.verified = true;
+  user.otp = undefined;
+  await user.save({ new: true, validateModifiedOnly: true });
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    message: "OTP verified Successfully!",
+    token,
+    user_id: user._id,
+  });
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({
+      status: "error",
+      message: "Both email and password are required",
+    });
+  }
+
+  const user = await User.findOne({ email: email }).select("+password");
+  if (!user || (await user.correctPassword(password, user.password))) {
+    res.status(400).json({
+      status: "error",
+      message: "Email or Password is incorrect",
+    });
+  }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    message: "Logged in successfully",
+    token,
+  });
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
+
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+const http = require("http");
+const server = http.createServer(app);
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((connection) => {
+    console.log("DB connection is successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log(`App running on port ${port} ...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
