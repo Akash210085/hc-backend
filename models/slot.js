74907474da2baca79daab86e5671acacfff5f9ec
Akash Kumar <ak8923307022@gmail.com>
2024-06-30 17:08:27 +0530

    Added profile for doctors

---
+++ models/slot.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const slotSchema = new mongoose.Schema({
+  id: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  category: {
+    type: String,
+  },
+  doctorName: {
+    type: String,
+  },
+  certified: {
+    type: Boolean,
+  },
+  slots: [
+    {
+      type: String, // Assuming 'preferredSlot' is a Date
+    },
+  ],
+});
+
+const Slot = new mongoose.model("Slot", slotSchema);
+module.exports = Slot;
